78a715eb612f90fd09692fbccc1cedbb6a08c2f8
Aayush Gupta <aayushgupta219@gmail.com>
2024-05-10 22:25:24 +0530

    Catima: Enable kotlin support
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 
 plugins {
     id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("com.github.spotbugs") version "5.1.4" apply false
 }
 
